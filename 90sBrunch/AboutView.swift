c948c14dac0b2ded98b35cd881679c3352181d9f
DS4506 <davidsmith4506@gmail.com>
2025-07-05 01:22:44 -0500

    90sBrunchsession3

--- 90sBrunch/AboutView.swift
+++ 90sBrunch/AboutView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct AboutView: View {
+    @State private var orders = 10
+    @State private var userName = "GeniusDave"
+    @State private var reservations = 0
+    
     var body: some View {
         VStack{
-            Text("Welcome to 90s Brunch")
+            Text("Welcome \(userName) to 90s Brunch")
                 .font(.title)
                 .padding()
             Image("littleLemonLogo")
                 .resizable()
                 .scaledToFit()
                 .frame(height: 200)
+            Text("You're order \(orders) times")
+            Button("Place Order"){
+                orders += 1
+            }
+            Button("reset"){
+                orders = 0
+        }
+            TextField(" Enter your name",text:$userName)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Button("Add people to the reservations"){ reservations += 1}
+            Text("Hi \(userName), your reservation number is \(orders) Thank you for your patiences")
+                .font(.caption)
+                .padding()
+            Text(String(repeating: "🍋", count: reservations))
+            Stepper("Guests: \(reservations)", value: $reservations, in: 1...10)
         }
         .padding()
         .navigationTitle("About us")
---
+++ practice/MyPlayground.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+
+//ver-variables (mmutable)
+//let- constants (same)
+
+var greeting = "Hello, playground"
+let day = "Monday"
+let dailyTemperature = 75
+print("Today is \(day) and the temperature is \(dailyTemperature) F")
+var temperature = 70
+print("The temperature in the morning was \(temperature)F")
+temperature = 88
+print("The temperature in the noon was \(temperature)F")
+temperature = dailyTemperature
+print("The temperature in the evening was \(temperature)F")
+      
+print("The game score example")
+let levelScore=10
+      var gameScore=0
+      
+gameScore+=levelScore
+      
+print("Your current score is \(gameScore)") // 10
+      var highScore=20.0
+print("Your bonus is \(levelBonusScore)")
+let levelLowestScore=50
+let levelHighScore=99
+      
+      
+      
+      
+      
+      
+      
+      
+            
+        
+      
+
+
+
+
